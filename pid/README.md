4bf204a1dd6ac9f0d983247aa9e6f4c8bb161ea3
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2023-09-10 20:25:02 -0300

    create pid.ino
    implementation of a discrete pid controller in C++ for Arduino

---
+++ pid/README.md
@@ -0,0 +1,3 @@
+# Controller
+
+Controller for a dynamic system written in C++ for Arduino
---
+++ pid/pid.ino
@@ -0,0 +1,45 @@
+#define VOLTAGEREADPIN 0
+#define VOLTAGEWRITEPIN 3
+
+double ref = 1.0, yN = 0.0, uN_0 = 0.0, uN_1 = 0.0, errN_0 = 0.0, errN_1 = 0.0, errN_2 = 0.0;
+unsigned int pwmValue = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+};
+
+void loop()
+{
+  double yN = pwm2real(analogRead(VOLTAGEREADPIN));
+
+  errN_2 = errN_1;
+  errN_1 = errN_0;
+  errN_0 = ref - yN;
+
+  uN_1 = uN_0;
+  uN_0 = controller(uN_1, errN_0, errN_1, errN_2);
+
+  pwmValue = real2pwm(uN_0);
+
+  analogWrite(VOLTAGEWRITEPIN, pwmValue);
+
+  Serial.println("Control value: " + String(uN_0));
+  Serial.println("Reference: " + String(ref));
+  Serial.println("Output value: " + String(yN));
+};
+
+int real2pwm(double real)
+{
+  return (int)255 / 5 * max(0, min(real, 5));
+}
+
+double pwm2real(int pwm)
+{
+  return (double)5.0 / 1023 * pwm;
+}
+
+double controller(double uN_1, double errN_0, double errN_1, double errN_2)
+{
+  return uN_1 + 11.85 * errN_0 - 23.32 * errN_1 + 11.48 * errN_2;
+}
