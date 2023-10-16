e0e10ee179c81a2902f5b68be2240715fee70377
Nikolas <nikolas@nikolas.com>
2023-10-15 21:46:08 -0300

    multiple improvements

--- control_z/control_z.ino
+++ control_z/control_z.ino
@@ -1,32 +1,42 @@
+#include "/home/nikolas/Documents/GitHub/Controller/controller/controller.h"
+
 #define VOLTAGEREADPIN 0
 #define VOLTAGEWRITEPIN 3
+#define SAMPLINGRATE 1000000
 
-double ref = 1.0, systemOutput = 0.0, uN_0 = 0.0, uN_1 = 0.0, errN_0 = 0.0, errN_1 = 0.0, errN_2 = 0.0;
-unsigned int pwmValue = 0;
-char outputString[50];
+Measure *err = createMeasure();
+Measure *u = createMeasure();
+
+double ref = 1.0, systemOutput = 0.0, intervalSeconds = 0.0;
+unsigned long prev = micros(), curr = micros();
+char outputString[100];
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
 };
 
 void loop()
 {
-    double systemOutput = pwm2real(analogRead(VOLTAGEREADPIN));
+    curr = micros();
+    if (curr - prev >= SAMPLINGRATE)
+    {
+        intervalSeconds = (curr - prev) / 1e6;
+        double systemOutput = pwm2real(analogRead(VOLTAGEREADPIN));
 
-    errN_2 = errN_1;
-    errN_1 = errN_0;
-    errN_0 = ref - systemOutput;
+        // update previous and current values
+        err->prev = err->curr;
+        err->curr = ref - systemOutput;
 
-    uN_1 = uN_0;
-    uN_0 = controller(uN_1, errN_0, errN_1, errN_2);
+        u->prev = u->curr;
+        u->curr = 0.9656449798564605 * u->prev + 0.069255968776331 * err->curr - 0.06759113027113339 * err->prev; // numerically solve recurrence equation
 
-    pwmValue = real2pwm(uN_0);
+        analogWrite(VOLTAGEWRITEPIN, real2pwm(u->curr));
 
-    analogWrite(VOLTAGEWRITEPIN, pwmValue);
-
-    sprintf(outputString, "Control value: %.2lf\nOutput value: %.2lf\n", uN_0, systemOutput);
-    Serial.print(outputString);
+        sprintf(outputString, "Current error: %f\nControl value: %f\n----------------------------\n", err->curr, u->curr);
+        Serial.print(outputString);
+        prev = micros();
+    }
 };
 
 int real2pwm(double real)
@@ -37,9 +47,4 @@ int real2pwm(double real)
 double pwm2real(int pwm)
 {
     return (double)5.0 / 1023 * pwm;
-}
-
-double controller(double uN_1, double errN_0, double errN_1, double errN_2)
-{
-    return 0.9656449798564605 * uN_1 + 0.069255968776331 * errN_0 - 0.06759113027113339 * errN_1;
 }
