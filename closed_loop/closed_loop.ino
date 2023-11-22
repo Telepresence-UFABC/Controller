b63dde7de588aec600375615808bb715d172aac4
Nikolas <nikolas.avelino@aluno.ufabc.edu.br>
2023-11-22 15:47:45 -0300

    change logging

--- closed_loop/closed_loop.ino
+++ closed_loop/closed_loop.ino
@@ -3,16 +3,15 @@
 #define pinone 5
 #define pintwo 6
 #define MAXTIME 5000
-#define MAXSAMPLES 3000
 
-uint16_t adcValue = 0, count = 0;
-double error = 0, output = 0, ramp = 0, outputSamples[MAXSAMPLES], rampSamples[MAXSAMPLES];
-unsigned long curr, prev, time[MAXSAMPLES];
-char buffer[50];
+uint16_t adcValue = 0;
+double error = 0, output = 0, ramp = 0, outputSamples, rampSamples;
+unsigned long curr, prev, time;
 
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("Tempo, Referencia, Erro, Posicao");
 }
 
 void loop()
@@ -26,28 +25,5 @@ void loop()
     if (curr - prev > MAXTIME)
         prev = millis();
     curr = millis();
-    sprintf(buffer, "Referencia: %lf\nErro: %lf\n", ramp, error);
-    Serial.print(buffer);
-
-    // store time, reference and output until maximum amount
-    if (count < MAXSAMPLES)
-    {
-        time[count] = curr;
-        rampSamples[count] = ramp;
-        outputSamples[count] = output;
-        count++;
-    }
-    // print data and halt after data collection
-    else
-    {
-        Serial.println("Tempo, Rampa, Saida");
-        for (int i = 0; i < MAXSAMPLES; i++)
-        {
-            sprintf(buffer, "%lf, %lf, %lf\n", time[i], rampSamples[i], outputSamples[i]);
-            Serial.print(buffer);
-        }
-        while (true)
-        {
-        }
-    }
+    Serial.println(String(ramp) + ", " + String(ramp) + ", " + String(error > 0 ? error - 1: error + 1) + ", " + String(output));
 }
