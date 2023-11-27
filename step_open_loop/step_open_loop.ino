d370080b039917902b96580209a78d24be8f03c1
Nikolas <nikolas.avelino@aluno.ufabc.edu.br>
2023-11-27 18:50:37 -0300

    change sampling rate

--- step_open_loop/step_open_loop.ino
+++ step_open_loop/step_open_loop.ino
@@ -1,30 +1,31 @@
-#include "/home/nikolas/Documents/GitHub/Controller/controller/controller.h"
+#include "/home/nikolas/GitHub/Controller/controller/controller.h"
 
 #define pinone 5
 #define pintwo 6
-#define MAXTIME 25
+#define MAXTIME 1e3
 
 uint16_t adcValue = 0;
-double error = 0, prevOutput = 0, output = 0, speed, ref = 5, tempo;
+double error = 0, prevOutput = 0, output = 0, speed, ref = 5, deltaT, tempo;
 unsigned long curr, prev = 0;
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println("Tempo, Velocidade");
 }
 
 void loop()
 {
-    curr = millis();
+    curr = micros();
     adcValue = analogRead(0);
-    hBridgeWrite(pinone, pintwo, ref > 0 ? ref + 1 : ref - 1);
+    hBridgeWrite(pinone, pintwo, ref);
     if(curr-prev>MAXTIME){
-      tempo = (double)(curr-prev)/1000;
+      deltaT = (double)(curr-prev)/1e6;
+      tempo = (double)curr/1e6;
       prevOutput = output;
       output = (double)adcValue / 1023 * 5;
-      speed = (output-prevOutput)/tempo;
-      Serial.println(String((double)(millis())/1000, 6) + ", " + String(speed, 6));
+      speed = (output-prevOutput)/deltaT;
+      Serial.println(String(tempo, 6) + ", " + String(speed, 6));
       prev = curr;
     }
 }
