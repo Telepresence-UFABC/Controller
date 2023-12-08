4c176400e4b692a73ebe036953e6a0ec83d0a0ae
Nikolas <nikolas.avelino@aluno.ufabc.edu.br>
2023-12-08 18:56:54 -0300

    change controller, fix error formula

--- control_z/control_z.ino
+++ control_z/control_z.ino
@@ -1,4 +1,4 @@
-#include "/home/nikolas/Documents/GitHub/Controller/controller/controller.h"
+#include "/home/nikolas/GitHub/Controller/controller/controller.h"
 
 #define VOLTAGEREADPIN 0
 #define pinone 5
@@ -9,13 +9,14 @@ Measure *err = createMeasure();
 Measure *u = createMeasure();
 
 uint16_t adcValue = 0;
-double output, time, ref = 3.5;
+double output, time, ref = 1;
 unsigned long curr, prev;
 char outputString[100];
 
 void setup()
 {
     Serial.begin(9600);
+    Serial.println("Erro,Esforco");
 };
 
 void loop()
@@ -23,21 +24,21 @@ void loop()
     curr = micros();
     if (curr - prev >= SAMPLINGRATE)
     {
-        time = (curr - prev) / 1e6;
+        time = (double)(curr - prev) / 1e6;
         adcValue = analogRead(VOLTAGEREADPIN);
         output = (double)adcValue/1023*5;
 
         // update previous and current values
         err->prev = err->curr;
         err->curr = ref - output;
-        err->curr = err->curr > 0 ? err->curr + 1 : err->curr - 1; // motor starts to run at about 1V
+        err->curr = err->curr;
 
         u->prev = u->curr;
-        u->curr = 0.13403841905010236 * u->prev + 4499.797360001363 * err->curr - 3339.5093253347313 * err->prev; // numerically solve recurrence equation
+        u->curr = 0.904754387023486 * u->prev + 16.913131432044782 * err->curr - 15.675594439833445 * err->prev; // numerically solve recurrence equation
 
         hBridgeWrite(pinone, pintwo, u->curr);
 
-        Serial.println(err->curr > 0 ? err->curr - 1 : err->curr + 1);
+        Serial.println(String(err->curr, 6) + "," + String(u->curr, 6));
         prev = micros();
     }
 };
