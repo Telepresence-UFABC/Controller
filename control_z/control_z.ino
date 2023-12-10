47162464d07d187af722e07a90da4c9daf630ae5
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2023-12-09 23:39:43 -0300

    create resetting every few seconds

--- control_z/control_z.ino
+++ control_z/control_z.ino
@@ -1,17 +1,25 @@
-#include "/home/nikolas/GitHub/Controller/controller/controller.h"
+#include "/home/nikolas/Documents/GitHub/Controller/controller/controller.h"
+#include <math.h>
 
 #define VOLTAGEREADPIN 0
 #define pinone 5
 #define pintwo 6
 #define SAMPLINGRATE 5000
+#define RESET_INTERVAL 5000000
+#define TOLERANCE 0.3
 
 Measure *err = createMeasure();
 Measure *u = createMeasure();
 
 uint16_t adcValue = 0;
-double output, time, ref = 2;
-unsigned long curr, prev;
-char outputString[100];
+double output, time, ref = 1;
+unsigned long curr, prev, prevReset;
+bool resetting = false;
+
+inline double control(Measure *err, Measure *u)
+{
+    return 0.904754387023486 * u->prev + 16.913131432044782 * err->curr - 15.675594439833445 * err->prev;
+}
 
 void setup()
 {
@@ -22,23 +30,42 @@ void setup()
 void loop()
 {
     curr = micros();
-    if (curr - prev >= SAMPLINGRATE)
+    if ((curr - prev >= SAMPLINGRATE) && !resetting)
     {
         time = (double)curr / 1e6;
         adcValue = analogRead(VOLTAGEREADPIN);
-        output = (double)adcValue/1023*5;
+        output = (double)adcValue / 1023 * 5;
 
         // update previous and current values
         err->prev = err->curr;
         err->curr = ref - output;
-        err->curr = err->curr;
 
         u->prev = u->curr;
-        u->curr = 0.904754387023486 * u->prev + 16.913131432044782 * err->curr - 15.675594439833445 * err->prev; // numerically solve recurrence equation
+        u->curr = control(err, u); // numerically solve recurrence equation
 
         hBridgeWrite(pinone, pintwo, u->curr);
 
         Serial.println(String(time, 6) + "," + String(output, 6) + "," + String(err->curr, 6) + "," + String(u->curr, 6));
         prev = micros();
+    }
+    if (curr - prevReset >= RESET_INTERVAL) // resets motor to 0 position every few seconds
+    {
+        resetting = true;
+        adcValue = analogRead(VOLTAGEREADPIN);
+        output = (double)adcValue / 1023 * 5;
+
+        // update previous and current values while setting reference to zero
+        err->prev = err->curr;
+        err->curr = -output;
+
+        u->prev = u->curr;
+        u->curr = control(err, u); // numerically solve recurrence equation
+
+        hBridgeWrite(pinone, pintwo, u->curr);
+        if (abs(output) <= TOLERANCE) // check if motor is at zero position, if it is start testing again
+        {
+            resetting = false;
+            prevReset = micros();
+        }
     }
 };
