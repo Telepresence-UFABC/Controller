55cd922eb184692d5ad1a576efb93b362c8607e5
Nikolas <nikolas.avelino@aluno.ufabc.edu.br>
2023-12-11 18:52:03 -0300

    motor stops running every few seconds for testing

--- step_open_loop/step_open_loop.ino
+++ step_open_loop/step_open_loop.ino
@@ -1,31 +1,47 @@
 #include "/home/nikolas/GitHub/Controller/controller/controller.h"
 
+#define VOLTAGEREADPIN 0
 #define pinone 5
 #define pintwo 6
-#define MAXTIME 1e3
+#define SAMPLINGRATE 5000
+#define RESET_INTERVAL 5000000
 
 uint16_t adcValue = 0;
-double error = 0, prevOutput = 0, output = 0, speed, ref = 5, deltaT, tempo;
-unsigned long curr, prev = 0;
+double error = 0, prevOutput = 0, output = 0, speed = 0, ref = 5, deltaT, time;
+unsigned long curr, prev, prevReset;
+unsigned char normalOperation = 1;
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Tempo, Velocidade");
+    Serial.begin(9600);
+    Serial.println("Tempo,Saida,Erro,Esforco");
 }
 
 void loop()
 {
     curr = micros();
-    adcValue = analogRead(0);
-    hBridgeWrite(pinone, pintwo, ref);
-    if(curr-prev>MAXTIME){
-      deltaT = (double)(curr-prev)/1e6;
-      tempo = (double)curr/1e6;
-      prevOutput = output;
-      output = (double)adcValue / 1023 * 5;
-      speed = (output-prevOutput)/deltaT;
-      Serial.println(String(tempo, 6) + ", " + String(speed, 6));
-      prev = curr;
+    if (curr - prev >= SAMPLINGRATE) // run at fixed sampling rate
+    {
+        adcValue = analogRead(VOLTAGEREADPIN);
+
+        // update previous and current values
+        prevOutput = output;
+        output = (double)adcValue / 1023 * 5;
+        time = (double)curr / 1e6;
+        deltaT = (double)(curr - prev) / 1e6;
+        speed = (output - prevOutput) / deltaT;
+
+        hBridgeWrite(pinone, pintwo, ref * normalOperation);
+        Serial.println(String(time, 6) + "," + String(speed, 6) + ",0,0");
+        prev = micros();
+    }
+    if (curr - prevReset >= RESET_INTERVAL) // stops motor every few seconds
+    {
+        normalOperation = 0;
+        if (curr - prevReset >= 1.5 * RESET_INTERVAL) // if enough time has passed, start normal operation again
+        {
+            normalOperation = 1;
+            prevReset = micros();
+        }
     }
 }
