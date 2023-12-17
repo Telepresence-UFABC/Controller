d1817fcba1e4223013e3b3985dbbec92a97abde2
Nikolas <nikolas@nikolas.com>
2023-12-17 14:33:38 -0300

    port controller.h to python

---
+++ raspberry_pi_controller/controller.py
@@ -0,0 +1,69 @@
+from pigpio import pi
+
+# System parameters
+VCC_RASPBERRY = 3.3
+V_OUT = 5
+PWM_FREQUENCY = 8_000  # Hz
+PWM_MAX_RANGE = 1000
+PIN_ONE = 17
+PIN_TWO = 22
+
+
+class Measure:
+    def __init__(self, prev=0, curr=0):
+        self.prev = prev
+        self.curr = curr
+
+
+def setup() -> pi:
+    rpi = pi()
+
+    # 0 <= PWM <= PWM_MAX_RANGE
+    pi.set_PWM_range(PIN_ONE, PWM_MAX_RANGE)
+
+    # Set PWM frequency
+    pi.set_PWM_frequency(PIN_ONE, PWM_FREQUENCY)
+    pi.set_PWM_frequency(PIN_TWO, PWM_FREQUENCY)
+    return rpi
+
+
+def voltage2angle(voltage: float) -> float:
+    """Converts potentiometer measured voltage to angle"""
+    # Max angle read by potentiometer is 300°
+    return voltage / VCC_RASPBERRY * 300
+
+
+def angle2voltage(angle: float, v_level: float = VCC_RASPBERRY) -> float:
+    """Converts angle to voltage, taking into account the appropriate voltage level"""
+    # Max angle read by potentiometer is 300°
+    return angle / 300 * v_level
+
+
+def h_bridge_write(rpi: pi, pin_one: int, pin_two: int, value: float) -> None:
+    """
+    ACT | P1 | P2
+
+    FWD | 1 | 0
+
+    BWD | 0 | 1
+
+    BRK | 0 | 0
+
+    BRK | 1 | 1
+    """
+    value = max(-V_OUT, min(V_OUT, value))
+
+    pwm = PWM_MAX_RANGE / V_OUT * abs(value)
+
+    # FORWARD
+    if value > 0:
+        rpi.set_PWM_dutycycle(pin_one, pwm)
+        rpi.set_PWM_dutycycle(pin_two, 0)
+    # FORWARD
+    elif value < 0:
+        rpi.set_PWM_dutycycle(pin_one, 0)
+        rpi.set_PWM_dutycycle(pin_two, pwm)
+    # BRAKE
+    else:
+        rpi.set_PWM_dutycycle(pin_one, 0)
+        rpi.set_PWM_dutycycle(pin_two, 0)
