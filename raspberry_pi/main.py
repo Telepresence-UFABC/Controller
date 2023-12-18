2632049a051b358ba54055c12a78873d48ab32b0
Nikolas <nikolas.avelino@aluno.ufabc.edu.br>
2023-12-18 18:27:55 -0300

    implement analog_read fix miscellaneous bugs

--- raspberry_pi/main.py
+++ raspberry_pi/main.py
@@ -1,7 +1,9 @@
 from time import time_ns
 from json import load
-from raspberry_pi.controller import *
+from Adafruit_ADS1x15 import ADS1115
+from controller import *
 
+VOLTAGE_READ_PIN = 0
 START = time_ns()
 # Run new iteration every SAMPLING_INTERVAL nanoseconds
 SAMPLING_INTERVAL = 5_000_000
@@ -9,13 +11,15 @@ SAMPLING_INTERVAL = 5_000_000
 RESET_INTERVAL = 2_000_000_000
 # Update reference every RECEIVE_INTERVAL nanoseconds
 RECEIVE_INTERVAL = 5_000_000
+# ADC gain set to GAIN
+GAIN = 1
 
 TOLERANCE = 5
 
 # Load ontroller constants
 with open("../system_parameters/controller_1.info", "r") as file:
     consts: dict = load(file)
-    C1, C2, C3 = consts["c1"], consts["c2"], consts["c3"]
+    C1, C2, C3 = float(consts["c1"]), float(consts["c2"]), float(consts["c3"])
 
 err = Measure()
 u = Measure()
@@ -27,9 +31,10 @@ prev_reset = 0
 prev_receive = 0
 normal_operation = 1
 
+adc = ADS1115()
 
-def analog_read(rpi: pi, pin: int = 0) -> float:
-    return 0
+def analog_read(pin: int = 0) -> float:
+    return max(adc.read_adc(pin, gain=GAIN), 0)/32767*4.096
 
 
 def control(err: Measure, u: Measure) -> float:
@@ -42,7 +47,7 @@ if __name__ == "__main__":
     while True:
         curr = time_ns()
         if curr - prev >= SAMPLING_INTERVAL:
-            output = voltage2angle(analog_read(rpi))
+            output = voltage2angle(analog_read(VOLTAGE_READ_PIN))
             time = (START - curr) / 1e9
 
             # Update previous and current values
@@ -52,7 +57,7 @@ if __name__ == "__main__":
             err.curr = ref_pan * normal_operation - output
             u.prev = u.curr
             u.curr = control(err, u)
-
+            
             h_bridge_write(rpi, PIN_ONE, PIN_TWO, u.curr)
 
             print("%.6f,%.6f,%.6f,%.6f" % (time, output, err.curr, u.curr))
